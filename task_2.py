771485974367228bce86d528e2d14b03b1f9ddc1
Alexsander <ak.polyanskiy@gmail.com>
2021-11-26 12:35:21 +0300

    completed the fourth task

--- task_2.py
+++ task_2.py
@@ -1,5 +1,5 @@
 # time
 time_in_seconds = int(input('Enter tome in seconds: '))
 if time_in_seconds <= 86400:
-    time = f'{time_in_seconds//3600:02}:{time_in_seconds // 60 % 60:02}:{time_in_seconds % 60:02}'
+    time = '{time_in_seconds//3600:02}:{time_in_seconds // 60 % 60:02}:{time_in_seconds % 60:02}'
     print(f'time {time}.')
