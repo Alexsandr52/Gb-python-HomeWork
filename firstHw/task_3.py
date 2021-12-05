f333a5c009d5bb19412d4e3923ee1cdd80d3e702
Alexsander <ak.polyanskiy@gmail.com>
2021-12-05 17:11:38 +0300

    fix some bags with git branch

--- firstHw/task_3.py
+++
@@ -1,5 +0,0 @@
-# sum
-user_number = input('Enter numder \'n\': ')
-result = int(user_number) + int(user_number + user_number) + \
-    int(user_number + user_number + user_number)
-print(f'{user_number} + {user_number*2} + {user_number*3} = {result}')
