f2ad76625ad66876ca3dd096aedd33732aa054ae
Alexsander <ak.polyanskiy@gmail.com>
2021-12-11 22:25:01 +0300

    finished homeWork

--- fourthHw/task_7.py
+++ fourthHw/task_7.py
@@ -1,16 +1,9 @@
-# 7. Реализовать генератор с помощью функции с ключевым словом yield, 
-# создающим очередное значение. При вызове функции должен создаваться объект-генератор. 
-# Функция должна вызываться следующим образом: for el in fact(n). Функция отвечает за получение факториала числа, 
-# а в цикле необходимо выводить только первые n чисел, начиная с 1! и до n!.
-# Подсказка: факториал числа n — произведение чисел от 1 до n. Например, факториал четырёх 4! = 1 * 2 * 3 * 4 = 24.
-def new_factorial(el):
-    counter = 1
-    help_list = [elem for elem in range(el)]
-    while counter < el:
-        yield 
-        counter += 1
+def fact(n):
+    for el in range(1, n + 1):
+        for elem in range(1, el):
+            el *= elem
+        yield el
 
-
-
-print()
+factorials = [el for el in fact(4)]
+print(factorials)
 
